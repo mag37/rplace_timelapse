802e852adaabb6855ced3928f3f21aa030ddbf0b
mag37 <robin.ivehult@gmail.com>
2023-09-10 21:55:07 +0200

    added montage-script

---
+++ montage_all.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Create output dir
+mkdir -p ALL
+starting_dir=$(pwd) # to get full path
+for dir in images2 images3 images4 images5 # iterate over subdirectories
+do
+  echo "Currently processing directory: $dir"
+  cd "${starting_dir}/${dir}" || { echo "No $dir directory, exiting." ; exit 1 ; }
+  if [[ $dir == "images2" ]] ; then  # different naming scheme for images2 - collection
+    ls -1 *.png | sed 's/^..//;s/....$//' | uniq > ${dir}.list
+  else 
+    ls -1 *.png | sed 's/......$//' | uniq > ${dir}.list
+  fi
+  while read -r line
+  do
+    for num in {0..5}
+    do
+      if [[ $dir == "images2" ]] ; then # different naming scheme for images2 - collection
+        [[ -f ./${num}-${line}.png ]] && declare canvas_${num}="./${num}-${line}.png" || declare canvas_${num}="xc:white"
+      else
+        [[ -f ./${line}-${num}.png ]] && declare canvas_${num}="./${line}-${num}.png" || declare canvas_${num}="xc:white"
+      fi
+    done
+    # create the montaged full canvas file
+    montage -verbose ${canvas_0} ${canvas_1} ${canvas_2} ${canvas_3} ${canvas_4} ${canvas_5} -geometry 1000x1000+0+0 ${starting_dir}/ALL/${line}.png
+  done < ${dir}.list # read the list of files
+done
+cd ${starting_dir} # return to base directory
